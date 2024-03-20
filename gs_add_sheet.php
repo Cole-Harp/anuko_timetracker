a47a5bda7ca182f8a8bf6d51835b53cb8acbc71e
ctstewart <8145799+ctstewart@users.noreply.github.com>
2024-03-20 10:57:55 -0500

    Changed some comments

--- gs_add_sheet.php
+++ gs_add_sheet.php
@@ -14,31 +14,17 @@ if (!(ttAccessAllowed('view_own_reports') || ttAccessAllowed('view_reports') || 
 }
 // End of access checks.
 
-// $mdb2 = getConnection();
+$mdb2 = getConnection();
 
-// $sql = "select spreadsheet_id from tt_google_sheets where user_id = $user->id";
-
-// $result = $mdb2->query($sql);
-
-// $dataArray = $result->fetchAll(MDB2_FETCHMODE_ASSOC);
-
-// $listOfSpreadsheets = [];
-// foreach ($dataArray as $row) {
-//   // Get the spreadsheet
-//   $spreadsheet = $service->spreadsheets->get($row['spreadsheet_id']);
-//   // Get the title of the spreadsheet
-//   $spreadsheetName = $spreadsheet->getProperties()->getTitle();
-//   // Name of spreadsheet
-//   $listOfSpreadsheets[$row['spreadsheet_id']] = $spreadsheetName;
-// }
+if ($request->isPost()) {
+  // Add logic to add a new row to the database,
+  // as well as make sure the id that was added is valid
+  // and has the proper permissions
+}
 
 $new_sheet_id = 'Hello';
 
 $form = new Form('googleSheetsForm');
-// $form->addInput(array('type'=>'combobox',
-//   'name'=>'sheetId',
-//   'data'=>$listOfSpreadsheets,
-//   'empty'=>array(''=>$i18n->get('dropdown.select'))));
 $form->addInput(array('type'=>'text','name'=>'newSheetId','value'=>$new_sheet_id));
 $form->addInput(array('type'=>'submit','name'=>'btn_send','value'=>'Next'));
 
