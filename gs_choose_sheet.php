2b902016d8f5d1407470b82a8462a03826e40883
ctstewart <8145799+ctstewart@users.noreply.github.com>
2024-03-20 10:44:09 -0500

    Added some google sheets logic

---
+++ gs_choose_sheet.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once('initialize.php');
+require_once('/var/www/html/vendor/autoload.php');
+require_once('gs_api_control.php');
+import('form.Form');
+import('form.ActionForm');
+import('ttReportHelper');
+
+// Access checks.
+if (!(ttAccessAllowed('view_own_reports') || ttAccessAllowed('view_reports') || ttAccessAllowed('view_all_reports')  || ttAccessAllowed('view_client_reports'))) {
+  header('Location: access_denied.php');
+  exit();
+}
+// End of access checks.
+
+if ($request->isPost()) {
+  header('Location: gs_add_sheet.php');
+}
+
+$mdb2 = getConnection();
+
+$sql = "select spreadsheet_id from tt_google_sheets where user_id = $user->id";
+
+$result = $mdb2->query($sql);
+
+$dataArray = $result->fetchAll(MDB2_FETCHMODE_ASSOC);
+
+$listOfSpreadsheets = [];
+foreach ($dataArray as $row) {
+  // Get the spreadsheet
+  $spreadsheet = $service->spreadsheets->get($row['spreadsheet_id']);
+  // Get the title of the spreadsheet
+  $spreadsheetName = $spreadsheet->getProperties()->getTitle();
+  // Name of spreadsheet
+  $listOfSpreadsheets[$row['spreadsheet_id']] = $spreadsheetName;
+}
+
+$form = new Form('googleSheetsForm');
+$form->addInput(array('type'=>'combobox',
+  'name'=>'sheetId',
+  'data'=>$listOfSpreadsheets,
+  'empty'=>array(''=>$i18n->get('dropdown.select'))));
+$form->addInput(array('type'=>'submit','name'=>'btn_send','value'=>'Next'));
+
+$smarty->assign('title', 'Time Tracker - Choose Spreadsheet');
+$smarty->assign('forms', array($form->getName()=>$form->toArray()));
+$smarty->assign('content_page_name', 'gs_choose_sheet.tpl');
+$smarty->display('index.tpl');
